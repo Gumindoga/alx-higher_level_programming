3ea0e498a0877a75cceeda48620aecb1e1536810
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-06 02:30:37 +0200

    fix: made exe

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,9 @@
-# Import the add function from add_0.py
 from add_0 import add
 
-# Assign the value 1 to a variable called 'a'
 a = 1
-
-# Assign the value 2 to a variable called 'b'
 b = 2
 
-# Use the add function to add 'a' and 'b'
 result = add(a, b)
 
-# Print the result using string formatting
-print(f"{a} + {b} = {result}")
+if __name__ == "__main__":
+	print(f"{a} + {b} = {result}")
