26f83b0b9f9236e92bce849b50f1a8d68c1f4f27
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-06 02:18:27 +0200

    py files: make executable

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+# Import the add function from add_0.py
+from add_0 import add
+
+# Assign the value 1 to a variable called 'a'
+a = 1
+
+# Assign the value 2 to a variable called 'b'
+b = 2
+
+# Use the add function to add 'a' and 'b'
+result = add(a, b)
+
+# Print the result using string formatting
+print(f"{a} + {b} = {result}")
