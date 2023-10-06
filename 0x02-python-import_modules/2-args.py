58458abb0c6787c1353a554e68f31ba9054f855f
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-06 03:04:30 +0200

    fix: function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 
-def main():
+
+if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("0 arguments.")
@@ -12,6 +13,3 @@ def main():
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-
-if __name__ == "__main__":
-    main()
