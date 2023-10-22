1b09a6c96bbb6232b2f5ae84e1358833dac1c4ed
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-22 22:00:46 +0200

    fix: errors

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join([chr(122 - i) if i % 2 == 0 else chr(90 - i) for i in range(26)]))
+print("".join([chr(122 - i).format() if i % 2 == 0 else chr(90 - i).format() for i in range(26)]))
