2103857c4845d3aeec13fba2620178f5954d224d
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-22 22:19:26 +0200

    fix: end of line error

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("".join([chr(122 - i).format() if i % 2 == 0 \
+print("".join([chr(122 - i).format() if i % 2 == 0
                else chr(90 - i).format() for i in range(26)]), end="")
