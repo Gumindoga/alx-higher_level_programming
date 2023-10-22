5e50db4fba35094db086d3f1c088fde3cab7fb51
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-10-22 15:36:06 +0200

    fix: pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,11 +3,11 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    int_val = 0
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    int_ = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            int_val += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and rom[roman_string[i]] > rom[roman_string[i - 1]]:
+            int_ += rom[roman_string[i]] - 2 * rom[roman_string[i - 1]]
         else:
-            int_val += roman_dict[roman_string[i]]
-    return int_val
+            int_ += rom[roman_string[i]]
+    return int_
