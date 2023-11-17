008c590d49031f446e0f81bce5df33df4c2881f0
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-17 22:06:48 +0200

    add: base files:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square.
+
+    Note:
+        This class doesn't do anything.
+
+    """
+
+    def __init__(self):
+        """Initializes a new instance of the Square class.
+
+        Note:
+            This method doesn't do anything.
+
+        """
+        pass
