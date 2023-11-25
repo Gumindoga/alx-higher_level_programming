4a3ebd475a3d222a42e12da45c8f11fb1e4256f4
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-25 15:15:30 +0200

    fix: blankline test case

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module 5-text_indentation
-Defines a function that prints a text with 2 new lines after each of these characters: ., ? and :
+Defines a function that prints a text with 2 new lines after: ., ? and :
 """
 
 
