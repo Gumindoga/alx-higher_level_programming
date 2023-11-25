9376a5b0b66eff984102931ba9b712ac672d571c
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-25 15:04:32 +0200

    add: task 4 files

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 5-text_indentation
+Defines a function that prints a text with 2 new lines after each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    Arguments:
+    text: string
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    print("\n".join([line.strip() for line in text.split('\n')]))
