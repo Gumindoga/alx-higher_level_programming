0d2365692e6dc56497698336fcd02e0e99c5f275
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-25 19:38:36 +0200

    style: pycode

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,7 +5,7 @@ Defines a function that multiplies 2 matrices
 """
 
 
-def matrix_mul(m_a, mb):
+def matrix_mul(m_a, m_b):
     """
     Multiplies 2 matrices
     Arguments:
@@ -15,38 +15,38 @@ def matrix_mul(m_a, mb):
     # Check if m_a and m_b are lists
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    if type(mb) is not list:
+    if type(m_b) is not list:
         raise TypeError("m_b must be a list")
 
     # Check if m_a and m_b are lists of lists
     if not all(isinstance(i, list) for i in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(i, list) for i in mb):
+    if not all(isinstance(i, list) for i in m_b):
         raise TypeError("m_b must be a list of lists")
 
     # Check if m_a and m_b are not empty
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if mb == [] or mb == [[]]:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
 
     # Check if each element of m_a and m_b is an integer or a float
     for row in m_a:
         if not all(isinstance(i, (int, float)) for i in row):
             raise TypeError("m_a should contain only integers or floats")
-    for row in mb:
+    for row in m_b:
         if not all(isinstance(i, (int, float)) for i in row):
             raise TypeError("m_b should contain only integers or floats")
 
     # Check if m_a and m_b are rectangles
     if not all(len(row) == len(m_a[0]) for row in m_a):
         raise TypeError("each row of m_a must be of the same size")
-    if not all(len(row) == len(mb[0]) for row in mb):
+    if not all(len(row) == len(m_b[0]) for row in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
     # Check if m_a and m_b can be multiplied
-    if len(m_a[0]) != len(mb):
+    if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     # Multiply m_a and m_b
-    return [[sum(a * b for a, b in zip(r, c)) for c in zip(*mb)] for r in m_a]
+    return [[sum(a * b for a, b in zip(r, c)) for c in zip(*m_b)] for r in m_a]
