030c256f5b3e260fa09106050f53f1fb421b12aa
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-25 14:08:43 +0200

    add: task 2 files

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 3-say_my_name
+Defines a function that prints "My name is <first name> <last name>"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints "My name is <first name> <last name>"
+    Arguments:
+    first_name: string
+    last_name: string
+    """
+    try:
+        if type(first_name) is not str:
+            raise TypeError("first_name must be a string")
+        if type(last_name) is not str:
+            raise TypeError("last_name must be a string")
+        print("My name is {} {}".format(first_name, last_name))
+    except NameError:
+        print("NameError: name '{}' is not defined".format(first_name))
