149ac3195a3d7e5515e7ef1bbb811cd05a6cb968
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-27 02:00:37 +0200

    add: all task files, ready for test

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is the "101-locked_class" module.
+
+The 101-locked_class module defines the LockedClass class.
+"""
+
+class LockedClass:
+    """
+    This is the LockedClass class.
+
+    The LockedClass class prevents the user
+    from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name.
+    """
+
+    __slots__ = ['first_name']
