0d7b9631a1d8f7f7a9c6ded84022f49f4ce8be2e
Blessed_B <blessed.muvindi@students.uz.ac.zw>
2023-11-27 22:54:36 +0200

    fix: index out of range + pycodestyle

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -13,15 +13,27 @@ def compute_metrics():
     This function reads from stdin line by line, splits each line into words,
     and extracts the status code and file size from the end of each line.
     It keeps a running total of the file size and counts of each status code.
-    Every 10 lines, it prints the total file size and counts of each status code.
+    Every 10 lines it prints the total file size & counts of each status code.
     It also handles a KeyboardInterrupt (CTRL + C) gracefully, printing the
     metrics before exiting.
     """
-    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    status_codes = {
+        200: 0,
+        301: 0,
+        400: 0,
+        401: 0,
+        403: 0,
+        404: 0,
+        405: 0,
+        500: 0
+    }
+    
     file_size = 0
     try:
         for i, line in enumerate(sys.stdin, 1):
             split_line = line.split()
+            if len(split_line) < 2:
+                continue
             status_code = int(split_line[-2])
             file_size += int(split_line[-1])
             if status_code in status_codes:
